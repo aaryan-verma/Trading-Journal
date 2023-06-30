1aebf660291537b693f7927c359df1c7d6b28dd6
Aaryan Verma <aryanverma.av2000@gmail.com>
2023-06-30 19:51:48 +0530

    some changes for vercel deployment

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const methodOverride = require('method-override');
-const connectDB = require('./server/config/db');
+const connectDB = require('../server/config/db');
 const session = require('express-session');
 const passport = require('passport');
 const MongoStore = require('connect-mongo');
@@ -41,9 +41,9 @@ app.set('layout','./layouts/main');
 app.set('view engine', 'ejs');
 
 //Routes
-app.use('/',require('./server/routes/auth'));
-app.use('/',require('./server/routes/index'));
-app.use('/',require('./server/routes/dashboard'));
+app.use('/',require('../server/routes/auth'));
+app.use('/',require('../server/routes/index'));
+app.use('/',require('../server/routes/dashboard'));
 
 
 
